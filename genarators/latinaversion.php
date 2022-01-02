f569ccf8d80238ba65690bec2fac2b8bfc455c37
Sajjad Ranjbar <83742735+phantomf4321@users.noreply.github.com>
2022-01-03 00:02:00 +0330

    Add files via upload

---
+++ genarators/latinaversion.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+		<title>مقاله ساز</title>
+		<meta charset="utf-8">		
+		<link rel="stylesheet" href="minified/themes/default.min.css" id="theme-style" />
+		<link rel="stylesheet" type="text/css" href="bootstrap4/css/bootstrap.min.css">
+        <link rel="stylesheet" href="ui.css">
+		<script src="minified/sceditor.min.js"></script>
+		<script src="minified/icons/monocons.js"></script>
+		<script src="minified/formats/bbcode.js"></script>
+		
+	</head>
+	<body>
+    <div class="svg-container">
+       <svg viewbox="0 0 800 400" class="svg">
+         <path id="curve" fill="#50c6d8" d="M 800 300 Q 400 350 0 300 L 0 0 L 800 0 L 800 300 Z"></path>
+       </svg>
+    </div>
+
+  <header>
+    <h1>سامانه مقاله ساز</h1>
+    <h3>نسخه لاتين</h3>
+  </header>
+  
+  <?php include("fields.php"); ?>
+	</body>
+</html>
